ffe0dbf58ad29ce878581233a7de05862037580f
TheFizzzz <huntdominique2004@gmail.com>
2026-04-26 23:39:56 -0500

    Looping through an array

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,70 @@
 console.log("Hello, world!");
 
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+//Initial Array of cards ^
+
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button",
+);
 
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
-
-
 editProfileButton.addEventListener("click", function () {
-    editProfileModal.classList.add("modal_is-opened")
+  editProfileModal.classList.add("modal_is-opened");
 });
 
-
 editProfileCloseButton.addEventListener("click", function () {
-    editProfileModal.classList.remove("modal_is-opened")
+  editProfileModal.classList.remove("modal_is-opened");
 });
 
 newPostButton.addEventListener("click", function () {
-    newPostModal.classList.add("modal_is-opened")
+  newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseButton.addEventListener("click", function () {
-    newPostModal.classList.remove("modal_is-opened")
+  newPostModal.classList.remove("modal_is-opened");
 });
 
+// Modals are for the button functionality
 
+initialCards.forEach(function (card) {
+  console.log(card.name);
+  console.log(card.link);
+});
+
+//this array is for calling initial cards to setup a for each loopthrough the initial cards array.
+//
